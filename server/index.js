1ea397573ebb6a91958596205f3a988fef9af5ca
Andrew Elliott <andrewelliott159@gmail.com>
2022-02-24 14:14:39 -0700

    Added Rollbar implementation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const app = express();
+
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+    accessToken: process.env.ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
+
+rollbar.log("Hello world!");
 
 app.use(express.static(path.join(__dirname, "/../client")));
 
